8d27c99fc7ad2feabee32de55718a5f395611b6e
Jason Wie Jonathan <jasonwiejo7@gmail.com>
2022-05-22 22:51:22 +0700

    Second Push

--- app/src/main/java/com/example/darurat/MainActivity.java
+++ app/src/main/java/com/example/darurat/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.darurat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), login.class));
+                finish();
+            }
+            }, 3000);
     }
 }
